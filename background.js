ca4e55d75c606939d70fdd78c40147d4de104363
szemod <75943279+szemod@users.noreply.github.com>
2025-02-08 16:09:09 +0100

    Update background.js

--- background.js
+++ background.js
@@ -21,8 +21,31 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         cookieString += `${cookie.name}=${cookie.value}; `;
       });
       console.log('Cookie String:', cookieString); // Naplózzuk a cookie stringet
-      sendResponse({ cookieString });
+
+      if (message.saveToFile) {
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+          chrome.tabs.sendMessage(tabs[0].id, {cookieString: cookieString});
+        });
+        sendResponse({ saved: true });
+      } else {
+        sendResponse({ cookieString });
+      }
     });
     return true; // Indicates that the response is asynchronous.
   }
 });
+
+// Automatikus cookie mentés a YouTube Music oldal betöltésekor
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url && tab.url.includes('music.youtube.com') && changeInfo.status === 'complete') {
+    chrome.cookies.getAll({ domain: '.youtube.com' }, (cookies) => {
+      console.log('Cookies:', cookies);
+      let cookieString = '';
+      cookies.forEach(cookie => {
+        cookieString += `${cookie.name}=${cookie.value}; `;
+      });
+      chrome.tabs.sendMessage(tabId, {cookieString: cookieString});
+    });
+  }
+});
+
